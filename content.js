f76b5e71acd38f65baa8436aa39ee9a02cf50fe4
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-03-16 17:33:31 -0700

    hrefs dont link to same href

--- content.js
+++ content.js
@@ -9,5 +9,8 @@ let urls = document.querySelectorAll('a');
 
 for (let i = 0; i < urls.length; i++) {
   let idx = Math.floor(Math.random() * linksArr.length);
+  while (urls[i] === linksArr[idx] && linksArr.length !== 1) {
+    idx = Math.floor(Math.random() * linksArr.length);
+  }
   urls[i].setAttribute('href', linksArr[idx])
 };
